f07eb4f4731131d9b8f83b08a709f2a1b87d2da6
Amyboid <134444582+Amyboid@users.noreply.github.com>
2025-04-25 01:11:01 +0530

    Add custom runner

---
+++ backend/runner-template.py
@@ -0,0 +1,25 @@
+import subprocess
+import signal
+import os
+import sys
+
+
+# Function to handle the SIGINT signal
+def signal_handler(sig, frame):
+    print("Signal received, terminating subprocess...")
+    if process:
+        process.terminate()  # Terminate the subprocess
+    sys.exit(0)  # Exit the script
+
+
+# Set up the signal handler
+signal.signal(signal.SIGINT, signal_handler)
+
+# Run the command as it is
+POSTGRES_URL = ""
+process = subprocess.Popen(
+    f"env POSTGRES_URL='{POSTGRES_URL}' fastapi dev app/main.py", shell=True
+)
+
+# Wait for the subprocess to complete
+process.wait()
