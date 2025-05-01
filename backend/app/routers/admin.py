b1b111d450f45e4731fba94b7c6ed18cd0789177
Amyboid <134444582+Amyboid@users.noreply.github.com>
2025-05-01 23:35:51 +0530

    Add new admin routes

--- backend/app/routers/admin.py
+++ backend/app/routers/admin.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter
+from fastapi.exceptions import HTTPException
 from pydantic import BaseModel
 
-from app.database import Admin, Admin_Pydantic
+from app.database import Admin, Patient, Doctor
 
 router = APIRouter(prefix="/admin", tags=["admin"])
 
@@ -10,10 +11,48 @@ class AdminCreatedResponse(BaseModel):
     msg: str
 
 
-@router.get("/me")
-async def read_profile_data():
-    # data = await Admin.create(username="demouser", name="Demo User", password_hash="securepasshash")
-    # print(data)
-    # return await Admin_Pydantic.from_tortoise_orm(data)
-    return {"msg": "admin created"}
+@router.get("/me/{id}")
+async def read_profile_data(id):
+    admin_data = await Admin.get_or_none(id=id).values()
+    if not admin_data:
+        raise HTTPException(status_code=404,detail="Admin not found")
+    return admin_data
 
+
+
+# /admin/patient 
+
+@router.get("/patient/{id}")
+async def read_patient_data(id):
+    patient_data = await Patient.get_or_none(id=id).values()
+    if not patient_data:
+        raise HTTPException(status_code=404, detail="Patient not found")
+    return patient_data
+
+@router.delete("/patient/{id}")
+async def delete_patient(id):
+    patient = await Patient.get_or_none(id=id)
+    if not patient:
+        raise HTTPException(status_code=404, detail="Patient not found")
+    await patient.delete()
+    
+    return {"msg": "Patient deleted successfully", "patient_id": id}
+
+
+# /admin/doctor 
+@router.get("/doctor/{id}")
+async def read_doctor_data(id):
+    doctor_data = await Doctor.get_or_none(id=id).values()
+    if not doctor_data:
+        raise HTTPException(status_code=404, detail="Doctor not found")
+    return doctor_data
+
+
+@router.delete("/doctor/{id}")
+async def delete_doctor(id):
+    doctor = await Doctor.get_or_none(id=id)
+    if not doctor:
+        raise HTTPException(status_code=404, detail="Doctor not found")
+    await doctor.delete()
+    
+    return {"msg": "doctor deleted successfully", "doctor_id": id}
