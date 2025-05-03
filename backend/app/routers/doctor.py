372d55270aefba0d4cd824c3f5016f2a475f1fb9
nextpointer <110530249+nextpointer@users.noreply.github.com>
2025-05-03 18:35:47 +0530

    add route /doctor/me (POST)

--- backend/app/routers/doctor.py
+++ backend/app/routers/doctor.py
@@ -15,8 +15,34 @@ router = APIRouter(prefix="/doctor", tags=["doctor"])
 @router.get(
     path = "/me",response_model=Doctor_Pydantic)
 async def get_doctor_data():
-    # fetch the doctor
-    doctor = await Doctor.get_or_none(id=1).values()
-    if not doctor:
-        raise HTTPException(status_code=404,detail="Doctor not found")
-    return doctor
+    try:
+        # fetch the doctor
+        doctor = await Doctor.get_or_none(id=1).values()
+        if not doctor:
+            raise HTTPException(status_code=404, detail="Doctor not found")
+        return doctor
+    except Exception as e:
+        raise HTTPException(status_code=422, detail=str(e))
+
+# model for create doctor
+class DoctorCreateData(BaseModel):
+    name: str
+    email: str
+    phone: str
+    specialization: str
+
+
+# route /me(POST)
+@router.post(path="/me")
+async def add_doctor(doctor_data:DoctorCreateData):
+    try:
+        await Doctor.create(
+            name=doctor_data.name,
+            email=doctor_data.email,
+            phone=doctor_data.phone,
+            specialization=doctor_data.specialization,
+        )
+        return {"msg": "doctor created"}
+    except Exception as e:
+        raise HTTPException(status_code=422, detail=str(e))
+
